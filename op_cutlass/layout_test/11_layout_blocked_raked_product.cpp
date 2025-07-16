c80c7161c68e73084bc26597404beee7c0bc0f28
atransient <just.number.n@gmail.com>
2025-07-16 17:32:43 +0800

    cutlass, cuda search demo develop

--- op_cutlass/layout_test/11_layout_blocked_raked_product.cpp
+++ op_cutlass/layout_test/11_layout_blocked_raked_product.cpp
@@ -4,7 +4,6 @@ using namespace cute;
 
 int main()
 {
-    print("=============== blocked_product ===============\n");
 
     Layout layout_A1 = make_layout(make_shape(Int<2>{},Int<2>{}),make_stride(Int<4>{},Int<1>{}));
     Layout layout_B1 = make_layout(make_shape(Int<6>{}),make_stride(Int<1>{}));
@@ -22,4 +21,11 @@ int main()
     print("\tblocked_product:   ");print(blocked_product(layout_A2,layout_B2));print("\n");
     print("\traked_product  :   ");print(raked_product(layout_A2,layout_B2));print("\n");print("\n");
 
+    Layout layout_A3 = make_layout(make_shape(Int<2>{},Int<5>{}),make_stride(Int<1>{},Int<2>{}));
+    Layout layout_B3 = make_layout(make_shape(Int<3>{},Int<4>{}),make_stride(Int<1>{},Int<3>{}));
+    print("layout_A:  ");print(layout_A3);print("\n");
+    print("layout_B:  ");print(layout_B3);print("\n");
+    print("\tlogical_product:   ");print(logical_product(layout_A3,layout_B3));print("\n");
+    print("\tblocked_product:   ");print(blocked_product(layout_A3,layout_B3));print("\n");
+    print("\traked_product  :   ");print(raked_product(layout_A3,layout_B3));print("\n");print("\n");
 }
---
+++ op_cutlass/search/pipeline_smem.cu
@@ -0,0 +1,147 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cassert>
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include "cutlass/cluster_launch.hpp"
+
+#include <cute/tensor.hpp>
+
+#include "cutlass/pipeline/pipeline.hpp"
+#include "cutlass/util/debug.h"
+#include "cutlass/device_kernel.h"
+
+using namespace cute;
+
+template <class ElementA,
+          class SmemLayoutA,  // (M,K,P)
+          uint32_t Stages>
+struct SharedStorage
+{
+    array_aligned<ElementA, cosize_v<SmemLayoutA>> smem_A;
+    typename cutlass::PipelineTmaAsync<Stages>::SharedStorage storage;
+};
+
+template <class ProblemShape, class CtaTiler, class TA, class SmemLayoutA, class TmaA, uint32_t NumStages>
+__global__ void test0(ProblemShape gmem_shape, CtaTiler cta_tiler, TA const *A, CUTLASS_GRID_CONSTANT TmaA const tma_a)
+{
+    //gmem tensor init
+    Tensor mA = tma_a.get_tma_tensor(gmem_shape);
+    Tensor gA = zipped_divide(mA, cta_tiler);
+
+    // //smem init
+    extern __shared__ char shared_memory[];
+    using SharedStorage = SharedStorage<TA, SmemLayoutA, NumStages>;
+    SharedStorage& smem = *reinterpret_cast<SharedStorage*>(shared_memory);
+    Tensor sA = make_tensor(make_smem_ptr(smem.smem_A.data()), SmemLayoutA{});
+
+    auto [tAgA, tAsA] = tma_partition(tma_a, Int<0>{}, Layout<_1>{},
+                                        group_modes<0,2>(sA), gA);
+    
+
+    //leader thread elect and warp id get
+    int warp_idx = __shfl_sync(0xffffffff, threadIdx.x / 32, 0);
+    int lane_predicate = cute::elect_one_sync();
+    //pipeline class define
+    using MainloopPipeline = typename cutlass::PipelineTmaAsync<NumStages>;
+    using PipelineState = typename cutlass::PipelineState<NumStages>;
+    using BarrierType = typename MainloopPipeline::ProducerBarrierType;
+    //pipeline init
+    typename MainloopPipeline::Params params;
+    uint32_t const TmaTransactionBytes = sizeof(TA) * size(cta_tiler);
+    if (thread0()) {
+        print("mA  : "); print(mA); print("\n");
+        print("gA  : "); print(gA); print("\n");
+        print("sA  : "); print(sA); print("\n");
+        print("tAgA  : "); print(tAgA); print("\n");
+        print("tAsA  : "); print(tAsA); print("\n");
+        print("TmaTransactionBytes  : "); print(TmaTransactionBytes); print("\n");
+        print("---------------------\n");
+    }
+    params.transaction_bytes = TmaTransactionBytes;
+    params.is_leader = (lane_predicate && warp_idx == 0);
+    params.num_consumers = blockDim.x;
+    MainloopPipeline pipeline(smem.storage, params, Shape<_1, _1, _1>{});
+
+    cute::cluster_arrive_relaxed();
+    cute::cluster_wait();
+    __syncthreads();
+    uint32_t num_iterations = size<1>(tAgA);
+    uint32_t tile_count = num_iterations;
+    PipelineState smem_pipe_write = cutlass::make_producer_start_state<MainloopPipeline>();
+    PipelineState smem_pipe_read;
+    if (lane_predicate && (warp_idx == 0))
+    {
+        // print("smem_pipe_write:  ");print(smem_pipe_write);print("\n");
+        int prologue_iterations = min(NumStages, num_iterations);
+        for ( int i = 0; i < prologue_iterations; ++i)
+        {
+            pipeline.producer_acquire(smem_pipe_write);
+            BarrierType* tma_barrier = pipeline.producer_get_barrier(smem_pipe_write);
+            // Can also specify stage to commit directly
+            copy(tma_a.with(*tma_barrier), tAgA(_,i), tAsA(_,i));
+            // pipeline.producer_commit(smem_pipe_write);
+            
+            --tile_count;
+            ++smem_pipe_write;
+        }
+    }
+    for (int i = 0; i < num_iterations; ++i)
+    {
+        pipeline.consumer_wait(smem_pipe_read);
+        __syncthreads();
+        uint read_index = smem_pipe_read.index();
+
+        //printf smem data
+        cutlass::debug::dump_shmem(raw_pointer_cast(sA(_,_,read_index).data()), size(cta_tiler));
+        pipeline.consumer_release(smem_pipe_read);
+
+        if (tile_count != 0 && lane_predicate && warp_idx == 0)
+        {
+            uint write_index = smem_pipe_write.index();
+            pipeline.producer_acquire(smem_pipe_write);
+            BarrierType* tma_barrier = pipeline.producer_get_barrier(smem_pipe_write);
+            // Can also specify stage to commit directly
+            copy(tma_a.with(*tma_barrier), tAgA(_, write_index), tAsA(_, write_index));
+            // pipeline.producer_commit(smem_pipe_write);
+            --tile_count;
+            ++smem_pipe_write;
+        }
+        ++smem_pipe_read;
+    }
+}
+
+
+template <class TA>
+void intermedia_config(int M, int N, TA const* A)
+{
+    auto gmem_shape = make_shape(M,N);
+    auto bM = Int<64>{};
+    auto bN = Int<32>{};
+    auto num_stage = Int<2>{};
+    auto dA = make_stride(Int<1>{}, M);
+    auto cta_tiler = make_shape(bM, bN);
+    
+    auto sA = tile_to_shape(GMMA::Layout_MN_SW128_Atom<TA>{}, make_shape(bM, bN, num_stage));
+    
+    Tensor mA = make_tensor(A, make_shape(M,N), dA);
+    Copy_Atom tmaA = make_tma_atom(SM90_TMA_LOAD{}, mA, sA(_,_,0), make_shape(bM,bN));
+    int smem_size = int(sizeof(SharedStorage<TA, decltype(sA), num_stage>));
+    dim3 block(128,1,1);
+    dim3 dimCluster(1, 1, 1);
+    dim3 grid(1,1,1);
+    test0<decltype(gmem_shape), decltype(cta_tiler), TA, decltype(sA), decltype(tmaA), num_stage><<<grid, block, smem_size>>>(gmem_shape, cta_tiler, A, tmaA);
+    CUTE_CHECK_LAST();
+}
+
+int main() {
+    int m = 128;
+    int n = 64;
+    using TA = int;
+    thrust::host_vector<TA> h_A(m*n);
+    for (int j = 0; j < m*n; ++j) h_A[j] = TA(int(j%1000));
+    thrust::device_vector<TA> d_A = h_A;
+    intermedia_config<TA>(m,n,d_A.data().get());
+    CUTE_CHECK_LAST();
+}
